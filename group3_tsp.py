1c25d476c7c35e4cd0095d97240132d86038d0f1
chowal <chowal@oregonstate.edu>
2018-06-07 10:26:47 -0700

    final draft ver. 1

--- group3_tsp.py
+++ group3_tsp.py
@@ -2,11 +2,11 @@
 # description: 
 #
 #
-#
 
 import argparse
 import math
 from collections import defaultdict
+import pdb
 
 
 def addEdge(graph, u, v):
@@ -23,7 +23,8 @@ def generate_edges(graph):
         # for each neighbour node of a single node
         for neighbour in graph[node]:
             # if edge exists then append
-            edges.append((node.cid, neighbour.cid))
+            edges.append((int(node.cid), int(neighbour.cid)))
+
     return edges
 
 
@@ -37,6 +38,7 @@ class city_obj():
         -> y = y coordinate
         -> neighbors = list of neighboring vertices
     """
+
     def __init__(self, city_id, x_coord, y_coord):
         self.cid = city_id
         self.priority = 1
@@ -51,10 +53,11 @@ class tsp():
         Travelling Salsemen Problem
         -> Utilizes Christofides algorithm
     """
+
     def __init__(self, t_file):
         self.data_file = open(t_file, "r")
         self.cities = []
-        
+
     def process_file(self):
         """
             Description:
@@ -65,14 +68,27 @@ class tsp():
         for city in self.data_file:
             city = city.strip('\n')
             # grabbing the identfier
-            city_id  = city.split(' ')[0]
+            city_id = city.split(' ')[0]
             # grabbing the coordinates
             coords = city.split(' ')[-2:]
-            # creating a new city object with id and coordinates 
-            new_city = city_obj(city_id, int(coords[0]), int(coords[1]))
+            # creating a new city object with id and coordinates
+            try:
+                new_city = city_obj(city_id, int(coords[0]), int(coords[1]))
+            except:
+                # compensating for lines with extraneous spaces
+                x = None
+                coords = city.split(' ')
+                del coords[0]
+                for i in coords:
+                    if i != "":
+                        if not x:
+                            x = i
+                        else:
+                            y = i
+                
+                new_city = city_obj(city_id, int(x), int(y))
             # adding the new city to the list of cities
             self.cities.append(new_city)
-
 
     def prims(self, city_list):
         """
@@ -88,9 +104,10 @@ class tsp():
             v.priority = self.distance(v, start)
             v.parent = start
         mst = defaultdict(list)
-        for i in range(len(city_list)-1):
+        for i in range(len(city_list) - 1):
             minimum = 9999999999999
-            minVertex = city_obj(0,0,0)
+            #minVertex = city_obj(0, 0, 0)
+            minVertex = start
             for v in city_list:
                 if v.priority > 0 and v.priority < minimum:
                     minimum = v.priority
@@ -103,29 +120,122 @@ class tsp():
                     v.priority = self.distance(v, minVertex)
                     v.parent = minVertex
 
-        print(generate_edges(mst))
+        return generate_edges(mst)
 
     def distance(self, c1, c2):
         """
             Calculates the distance between 2 ciites
-            Formula: 
+            Formula:
                 SQRT((x2 - x1)^2 + (y2 - y1)^2)
             Param:
                 c1, c2 == city objects
             Returns:
                 the distance between the 2 cities
         """
-        x = (c2.x - c1.x)**2
-        y = (c2.y - c1.y)**2
+        x = (c2.x - c1.x) ** 2
+        y = (c2.y - c1.y) ** 2
         d = math.sqrt(x + y)
         return d
-            
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description= "TSP Project - Christofides Algorithm")
-    #parser.add_argument('filename', help="Enter the file name as 1st arg")
-    #arg = parser.parse_args()
+
+def euler_tour(mst_set):
+    # Create a variable to keep track of the connected city set possibilities
+    city_sets = {}
+    # Go through each city pair and validate that we do not return / leave from the same city more than once
+    for connection in mst_set:
+        # If the city is not currently in the set, add it to it's "city number" as a blank set
+        if connection[0] not in city_sets:
+            city_sets[connection[0]] = []
+        # Also, if the secondary city is not in the set add it as a blank set as well
+        if connection[1] not in city_sets:
+            city_sets[connection[1]] = []
+        # Once validated, add the connected city into the previously created list sets
+        city_sets[connection[0]].append(connection[1])
+        city_sets[connection[1]].append(connection[0])
+
+    #print("City Sets: ", city_sets)
+
+    # Now find the shortest cycle between all of the cities
+
+    # Set the starting point to spot 0
+    start_vertex = mst_set[0][0]
+    # Start the beginning of the circuit at 0 as well
+    circuit = [city_sets[start_vertex][0]]
+    # Loop through the entire MST tree until we have no more available elements to pull from
+    while len(mst_set) > 0:
+        #
+        for i, curr_city in enumerate(circuit):
+            if len(city_sets[curr_city]) > 0:
+                break
+        # While the total number remaining cities is above 0...
+        while len(city_sets[curr_city]) > 0:
+            # Save the current city number temporarily
+            temp = city_sets[curr_city][0]
+            # Check for duplicate connections like it
+            connection_cleanup(mst_set, curr_city, temp)
+            # Remove city from the set so it cannot be used again
+            del city_sets[curr_city][(city_sets[curr_city].index(temp))]
+            del city_sets[temp][(city_sets[temp].index(curr_city))]
+
+            i += 1
+            # Add the previously deleted city back into the circuit
+            circuit.insert(i, temp)
+
+            curr_city = temp
+
+    return circuit
+
+
+def connection_cleanup(mst_set, v1, v2):
+    # This is to validate that there are no duplicate connections in our MST_SET
+    for i, item in enumerate(mst_set):
+        if (item[0] == v2 and item[1] == v1) or (item[0] == v1 and item[1] == v2):
+            del mst_set[i]
+
+    return mst_set
+
+def print_list(eul_list):
+    outfile = 'output.txt'
+    f = open(outfile, 'w')
+    ans = 0
+    for i in eul_list:
+        try:
+            ans += t.distance(t.cities[i], t.cities[i+1]) 
+        except:
+            # compensating for the last city which needs the distance to start
+            ans += t.distance(t.cities[i], t.cities[0]) 
+
+    f.write(str(int(ans))+ '\n')
+
+    for ele in eul_list:
+        f.write(str(ele) + '\n')
+
+    f.close()
+
     
-    t = tsp('tsp_example_1.txt')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="TSP Project - Christofides Algorithm")
+    parser.add_argument('filename', help="Enter the file name as 1st arg")
+    arg = parser.parse_args()
+
+    t = tsp(arg.filename)
     t.process_file()
-    t.prims(t.cities)
+
+    myList = t.prims(t.cities)
+    updList = euler_tour(myList)
+
+    current = updList[0]
+    path = [current]
+    visited = [False] * len(updList)
+
+    length = 0
+    for v in updList[1:]:
+        if not visited[v]:
+            path.append(v)
+            visited[v] = True
+
+            current = v
+
+    path.append(path[0])
+    #print("Path: ", *path)
+    print_list(updList)
